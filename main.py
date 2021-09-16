f3ef66276085deed48484b6554bd550d14d062d4
Nigel Davis <ngeldvis@gmail.com>
2021-09-16 14:51:51 -0400

    removed unnecessary comments and fixed spelling error

--- main.py
+++ main.py
@@ -1,43 +1,34 @@
 import cv2 as cv
 import numpy as np
 
-# keep track of the most recent mouse position after the user clicked their left mouse button
 class MousePosition():
 
     def __init__(self, x: int) -> None:
         self.pos = x
 
     def get_mouse_position(self, event, x: int, y: int, flags, params) -> None:
-        # Left Mouse Button Clicked
         if event == cv.EVENT_LBUTTONDOWN:
             self.pos = x
 
 
-# Video Capture Context Manager
 class VideoCaptureCM(cv.VideoCapture):
 
     def __enter__(self):
         return self
 
-    # release the camera resource
     def __exit__(self, type, value, traceback):
         self.release()
 
 
-# returns the frame but flipped over the given axis
-def get_argmented_img(frame: np.ndarray, w: int, h: int, x: int) -> np.ndarray:
-    # create an empty frame
+def get_augmented_img(frame: np.ndarray, w: int, h: int, x: int) -> np.ndarray:
     img = np.zeros((h, w, 3), np.uint8)
-
-    # check which side of the screen the user most recently clicked on
     left = True if x > w // 2 else False
-    if left: # flipping the left side onto the right side
+    if left:
         img[0:h, 0:x] = frame[0:h, 0:x]
         img[0:h, x:w] = np.flip(frame[0:h, 2*x-w:x], 1)
-    else: # flipping the right side onto the left side
+    else:
         img[0:h, x:w] = frame[0:h, x:w]
         img[0:h, 0:x] = np.flip(frame[0:h, x:x*2], 1)
-
     return img
     
 
@@ -46,29 +37,22 @@ def main() -> None:
 
     with VideoCaptureCM(0, cv.CAP_DSHOW) as capture:
         
-        # capture = cv.VideoCapture(0, cv.CAP_DSHOW)
         if not capture.isOpened():
             print("Cannot open camera")
             return
 
         width, height = int(capture.get(cv.CAP_PROP_FRAME_WIDTH)), int(capture.get(cv.CAP_PROP_FRAME_HEIGHT))
-        # set the initial axis position to half of the width of the screen
         mouse_position = MousePosition(width // 2)
 
         while True:
-            # continuously grab the image from the capture device
             ret, frame = capture.read()
             if not ret:
                 print("Can't receive frame")
                 break
 
-            # display the frame onto the window
-            cv.imshow('frame', get_argmented_img(frame, width, height, mouse_position.pos))
-
-            # check for any mouse click events
+            cv.imshow('frame', get_augmented_img(frame, width, height, mouse_position.pos))
             cv.setMouseCallback('frame', mouse_position.get_mouse_position)
 
-            # wait for either the 'q' check to be pressed or the X window button to be clicked to end the program
             if cv.waitKey(1) == ord('q') or cv.getWindowProperty('frame', cv.WND_PROP_AUTOSIZE) < 1:
                 break
 
